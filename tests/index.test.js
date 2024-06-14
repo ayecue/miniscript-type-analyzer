a68f0b3ce0e6a9244518792a845b3a85aa6dcc1a
ayecue <soerenwehmeier@googlemail.com>
2024-06-15 00:34:09 +0200

    add tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,10 +1,24 @@
 const { getDocument } = require('./utils');
 
 describe('type-manager', () => {
-  test('should return entity for property', () => {
+  test('should return entity for 1 level depth property', () => {
     const doc = getDocument(`test = "123"`);
     const scope = doc.getRootScopeContext().scope;
+
     expect(scope.resolveProperty('test').types.size).toEqual(1);
     expect(Array.from(scope.resolveProperty('test').types)).toEqual(['string']);
+  });
+
+  test('should return entity for 2 level depth property ', () => {
+    const doc = getDocument(`
+      test = {}
+      test.foo = 123
+    `);
+    const scope = doc.getRootScopeContext().scope;
+
+    expect(scope.resolveProperty('test').types.size).toEqual(1);
+    expect(Array.from(scope.resolveProperty('test').types)).toEqual(['map']);
+    expect(scope.resolveProperty('test').resolveProperty('foo').types.size).toEqual(1);
+    expect(Array.from(scope.resolveProperty('test').resolveProperty('foo').types)).toEqual(['number']);
   });
 });
