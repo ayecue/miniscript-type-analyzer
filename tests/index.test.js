1dfd89672b012e06920450357afccd9a9e6e74af
ayecue <soerenwehmeier@googlemail.com>
2024-06-15 02:47:24 +0200

    add tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -2,41 +2,88 @@ const { getDocument } = require('./utils');
 const { Entity } = require('../dist');
 
 describe('type-manager', () => {
-  test('should return entity for 1 level depth property', () => {
-    const doc = getDocument(`test = "123"`);
-    const scope = doc.getRootScopeContext().scope;
+  describe('1 level depth property', () => {
+    test('should return entity', () => {
+      const doc = getDocument(`test = "123"`);
+      const scope = doc.getRootScopeContext().scope;
 
-    expect(scope.resolveProperty('test').types.size).toEqual(1);
-    expect(Array.from(scope.resolveProperty('test').types)).toEqual(['string']);
+      expect(scope.resolveProperty('test').types.size).toEqual(1);
+      expect(Array.from(scope.resolveProperty('test').types)).toEqual(['string']);
+    });
+
+    test('should return entity with multiple types', () => {
+      const doc = getDocument(`
+        test = "123"
+        test = 123
+      `);
+      const scope = doc.getRootScopeContext().scope;
+
+      expect(scope.resolveProperty('test').types.size).toEqual(2);
+      expect(Array.from(scope.resolveProperty('test').types)).toEqual(['string', 'number']);
+    });
   });
 
-  test('should return entity for 2 level depth property ', () => {
-    const doc = getDocument(`
-      test = {}
-      test.foo = 123
-    `);
-    const scope = doc.getRootScopeContext().scope;
+  describe('2 level depth property', () => {
+    test('should return entity', () => {
+      const doc = getDocument(`
+        test = {}
+        test.foo = 123
+      `);
+      const scope = doc.getRootScopeContext().scope;
 
-    expect(scope.resolveProperty('test').types.size).toEqual(1);
-    expect(Array.from(scope.resolveProperty('test').types)).toEqual(['map']);
-    expect(scope.resolveProperty('test').resolveProperty('foo').types.size).toEqual(1);
-    expect(Array.from(scope.resolveProperty('test').resolveProperty('foo').types)).toEqual(['number']);
-  });
+      expect(scope.resolveProperty('test').types.size).toEqual(1);
+      expect(Array.from(scope.resolveProperty('test').types)).toEqual(['map']);
+      expect(scope.resolveProperty('test').resolveProperty('foo').types.size).toEqual(1);
+      expect(Array.from(scope.resolveProperty('test').resolveProperty('foo').types)).toEqual(['number']);
+    });
 
-  test('should return entity for 2 level depth property with index', () => {
-    const doc = getDocument(`
-      test = {}
-      test[222] = "hello"
-    `);
-    const scope = doc.getRootScopeContext().scope;
-    const numberKey = new Entity({
-      kind: -1,
-      document: doc
-    }).addType('number');
+    test('should return entity with multiple types', () => {
+      const doc = getDocument(`
+        test = {}
+        test.foo = 123
+        test.foo = []
+      `);
+      const scope = doc.getRootScopeContext().scope;
 
-    expect(scope.resolveProperty('test').types.size).toEqual(1);
-    expect(Array.from(scope.resolveProperty('test').types)).toEqual(['map']);
-    expect(scope.resolveProperty('test').resolveProperty(numberKey).types.size).toEqual(1);
-    expect(Array.from(scope.resolveProperty('test').resolveProperty(numberKey).types)).toEqual(['string']);
+      expect(scope.resolveProperty('test').types.size).toEqual(1);
+      expect(Array.from(scope.resolveProperty('test').types)).toEqual(['map']);
+      expect(scope.resolveProperty('test').resolveProperty('foo').types.size).toEqual(2);
+      expect(Array.from(scope.resolveProperty('test').resolveProperty('foo').types)).toEqual(['number', 'list']);
+    });
+
+    test('should return entity with index', () => {
+      const doc = getDocument(`
+        test = {}
+        test[222] = "hello"
+      `);
+      const scope = doc.getRootScopeContext().scope;
+      const numberKey = new Entity({
+        kind: -1,
+        document: doc
+      }).addType('number');
+
+      expect(scope.resolveProperty('test').types.size).toEqual(1);
+      expect(Array.from(scope.resolveProperty('test').types)).toEqual(['map']);
+      expect(scope.resolveProperty('test').resolveProperty(numberKey).types.size).toEqual(1);
+      expect(Array.from(scope.resolveProperty('test').resolveProperty(numberKey).types)).toEqual(['string']);
+    });
+
+    test('should return entity with index + multiple types', () => {
+      const doc = getDocument(`
+        test = {}
+        test[222] = "hello"
+        test[454] = {}
+      `);
+      const scope = doc.getRootScopeContext().scope;
+      const numberKey = new Entity({
+        kind: -1,
+        document: doc
+      }).addType('number');
+
+      expect(scope.resolveProperty('test').types.size).toEqual(1);
+      expect(Array.from(scope.resolveProperty('test').types)).toEqual(['map']);
+      expect(scope.resolveProperty('test').resolveProperty(numberKey).types.size).toEqual(2);
+      expect(Array.from(scope.resolveProperty('test').resolveProperty(numberKey).types)).toEqual(['string', 'map']);
+    });
   });
 });
